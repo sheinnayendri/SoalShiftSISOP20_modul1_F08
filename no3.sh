ddaae706666a8d584781211b8744ce6b1ff8134e
Rapuyy <56967667+Rapuyy@users.noreply.github.com>
2020-02-21 16:53:23 +0700

    Update no3.sh
    ngilangin echo echo ngetes eror

--- no3.sh
+++ no3.sh
@@ -8,8 +8,7 @@ mkdir /home/rapuyy/Downloads/tugas/prak1/no3/duplicate
 
 for ((a=1; a<=28; a=a+1))
 	do
-	wget -a /home/rapuyy/Downloads/tugas/prak1/no3/wget.log "https://loremflickr.com/320/240/cat" -O /home/rapuyy/Downloads/tugas/prak1/no3/pdkt_kusuma_"$a".jpeg #rapuyy buat temp
-#	cat rapuyy.txt >> wget.log
+	wget -a /home/rapuyy/Downloads/tugas/prak1/no3/wget.log "https://loremflickr.com/320/240/cat" -O /home/rapuyy/Downloads/tugas/prak1/no3/pdkt_kusuma_"$a".jpeg
 done
 
 grep "Location" /home/rapuyy/Downloads/tugas/prak1/no3/wget.log > /home/rapuyy/Downloads/tugas/prak1/no3/location.log
@@ -21,11 +20,11 @@ do
 	nomer=$(ls -1 /home/rapuyy/Downloads/tugas/prak1/no3/kenangan | wc -l)
 	nomer2=$(ls -1 /home/rapuyy/Downloads/tugas/prak1/no3/duplicate | wc -l)
 	flag=$((0))
-	echo ${arr[$a]}, $nomer ,$nomer2
-	echo pdkt_kusuma_"$(($a+1))".jpeg
+	#echo ${arr[$a]}, $nomer ,$nomer2 
+	#echo pdkt_kusuma_"$(($a+1))".jpeg
 	for((i=0; i<$a; i=i+1)) 
 		do 
-		echo perbandingan["$(($a+1))"]dengan["$(($i+1))"]
+		#echo perbandingan["$(($a+1))"]dengan["$(($i+1))"]
 		if [ $a -eq 0 ] 
 			then mv /home/rapuyy/Downloads/tugas/prak1/no3/pdkt_kusuma_1.jpeg kenangan/kenangan_1.jpeg
 			
