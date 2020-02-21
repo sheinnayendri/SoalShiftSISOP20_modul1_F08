68957c84952bbad6aaa36b31a5601c0da6f64d53
Rapuyy <56967667+Rapuyy@users.noreply.github.com>
2020-02-21 17:12:00 +0700

    fix parah nomer 3

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -1,32 +1,35 @@
 #! /bin/bash
 
-mkdir /home/rapuyy/Downloads/tugas/prak1/no3/kenangan
-mkdir /home/rapuyy/Downloads/tugas/prak1/no3/duplicate
+cd /home/rapuyy/Downloads/tugas/prak1/no3
 
- > /home/rapuyy/Downloads/tugas/prak1/no3/wget.log   
- > /home/rapuyy/Downloads/tugas/prak1/no3/location.log 
+mkdir kenangan
+mkdir duplicate
+
+ > wget.log   
+ > location.log 
 
 for ((a=1; a<=28; a=a+1))
 	do
-	wget -a /home/rapuyy/Downloads/tugas/prak1/no3/wget.log "https://loremflickr.com/320/240/cat" -O /home/rapuyy/Downloads/tugas/prak1/no3/pdkt_kusuma_"$a".jpeg
+	wget -a wget.log "https://loremflickr.com/320/240/cat" -O pdkt_kusuma_"$a".jpeg #rapuyy buat temp
+#	cat rapuyy.txt >> wget.log
 done
 
-grep "Location" /home/rapuyy/Downloads/tugas/prak1/no3/wget.log > /home/rapuyy/Downloads/tugas/prak1/no3/location.log
+grep "Location" wget.log > location.log
 
-readarray -t arr < /home/rapuyy/Downloads/tugas/prak1/no3/location.log
+readarray -t arr < location.log
 flag=0
 for((a=0; a<28; a=a+1)) 
 do
-	nomer=$(ls -1 /home/rapuyy/Downloads/tugas/prak1/no3/kenangan | wc -l)
-	nomer2=$(ls -1 /home/rapuyy/Downloads/tugas/prak1/no3/duplicate | wc -l)
+	nomer=$(ls -1 kenangan | wc -l)
+	nomer2=$(ls -1 duplicate | wc -l)
 	flag=$((0))
-	#echo ${arr[$a]}, $nomer ,$nomer2 
+	#echo ${arr[$a]}, $nomer ,$nomer2
 	#echo pdkt_kusuma_"$(($a+1))".jpeg
 	for((i=0; i<$a; i=i+1)) 
 		do 
 		#echo perbandingan["$(($a+1))"]dengan["$(($i+1))"]
 		if [ $a -eq 0 ] 
-			then mv /home/rapuyy/Downloads/tugas/prak1/no3/pdkt_kusuma_1.jpeg kenangan/kenangan_1.jpeg
+			then mv pdkt_kusuma_1.jpeg kenangan/kenangan_1.jpeg
 			
 		
 		elif [ "${arr[$a]}" == "${arr[$i]}" ] 
@@ -41,11 +44,14 @@ do
 	
 	if [ $flag -eq 0 ] 
 	then 
-		mv /home/rapuyy/Downloads/tugas/prak1/no3/pdkt_kusuma_"$(($a+1))".jpeg /home/rapuyy/Downloads/tugas/prak1/no3/kenangan/kenangan_"$(($nomer+1))".jpeg
-		echo case1
+		mv pdkt_kusuma_"$(($a+1))".jpeg kenangan_"$(($nomer+1))".jpeg
+		#echo case1
 	else 
-		echo case2
-		mv /home/rapuyy/Downloads/tugas/prak1/no3/pdkt_kusuma_"$(($a+1))".jpeg /home/rapuyy/Downloads/tugas/prak1/no3/duplicate/duplicate_"$(($nomer2+1))".jpeg
+		#echo case2
+		mv pdkt_kusuma_"$(($a+1))".jpeg duplicate_"$(($nomer2+1))".jpeg
 	fi
 
 done
+
+cat wget.log >> wget.log.bak
+cat location.log >> location.log.bak
